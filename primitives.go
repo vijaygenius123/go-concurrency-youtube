b7225998effa479e16ceafb167d15ac453658dba
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2022-06-14 17:31:51 +0100

    Channel example

--- primitives.go
+++ primitives.go
@@ -2,17 +2,26 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"time"
 )
 
-func someFunc(num string) {
-	fmt.Println(num)
+func someFunc(num string, channel chan string) {
+	sec, _ := strconv.Atoi(num)
+	time.Sleep(time.Duration(sec) * time.Second)
+	channel <- num
 }
 
 func main() {
-	go someFunc("1")
-	go someFunc("2")
-	go someFunc("3")
-	time.Sleep(time.Second * 2)
-	fmt.Println("Hi")
+	myChannel1 := make(chan string)
+	myChannel2 := make(chan string)
+	myChannel3 := make(chan string)
+	go someFunc("1", myChannel1)
+	go someFunc("2", myChannel2)
+	go someFunc("3", myChannel3)
+	//time.Sleep(time.Second * 2)
+	msg1 := <-myChannel1
+	msg2 := <-myChannel2
+	msg3 := <-myChannel3
+	fmt.Println(msg1, msg2, msg3)
 }
